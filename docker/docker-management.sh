cd7ec658467d91123569977e69dcc361aa7b3b78
Patricia Pereira <patricia.pereira@xpand-it.com>
2019-11-13 14:08:39 +0000

    Base react project

---
+++ docker/docker-management.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+IMAGE_NAME=node:12.8.1
+PROJECT_SRC="//c/MyProjects/typper-web"
+HOST_PORT=3001
+CONTAINER_NAME=typper-web-local
+
+#######################################################################################################################
+
+usage() {
+	echo
+	echo "Usage:  $0 COMMAND"
+	echo  
+	echo "Commands:"
+	echo "  start     Run a new container for this project. "
+	echo "  stop      Stop and remove the container created using \"start\"."
+	exit 0
+}
+
+[[ $# -eq 0 ]] && usage
+
+start() {
+	docker run -tid -w //usr/src -v "$PROJECT_SRC":/usr/src -p "$HOST_PORT":3000 -e CHOKIDAR_USEPOLLING=true --name "$CONTAINER_NAME" "$IMAGE_NAME" //bin/bash
+}
+
+stop () {
+	docker stop "$CONTAINER_NAME"
+	docker rm "$CONTAINER_NAME"
+}
+
+wrongCommand() {
+	echo "$(basename "$0"): '$1' is not a valid command."
+	echo "Run '$0' to view a list of the valid commands."
+	exit 1
+}
+
+case "$1" in
+    "start") start;;
+    "stop") stop;;
+	*) wrongCommand "$1";;
+esac
